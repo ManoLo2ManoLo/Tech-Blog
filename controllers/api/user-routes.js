e3d3be6233f3da9e1deefe90324b6df14f80ce58
ManoLo2ManoLo <manuel_canas@aol.com>
2021-12-03 05:01:01 -0500

    users can now create accounts

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,4 +2,38 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../Models')
 const withAuth = require('../../utils/auth');
 
+router.get('/', (req, res) => {
+    User.findAll()
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      
+      res.status(500).json(err);
+    });
+})
+
+router.post("/", (req, res) => {
+    User.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then((dbUserData) => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+  
+          res.json(dbUserData);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,20 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../Models')
 const withAuth = require('../utils/auth.js');
 
+router.get('/', (req, res) => {
+    res.render('homePage');
+});
+
 router.get('/login', (req, res) => {
     res.render('login');
 });
 
 router.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+router.get('/dashboard', withAuth, (req, res) => {
+    res.render('dashboard');
 });
 
 module.exports = router;
